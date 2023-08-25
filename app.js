1ced4e0cf3c47a52b7cf26d559b99112fc51a28c
Priscila Borges <priscila.borges.nl@gmail.com>
2023-08-25 17:06:36 +0200

    fake login form (req.body)

--- app.js
+++ app.js
@@ -1,73 +1,113 @@
 const express = require("express");
-const hbs = require("hbs")
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const hbs = require("hbs");
 const app = express();
+
+const Pizza = require("./models/Pizza.model");
+
 
 app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
 app.set("view engine", "hbs"); //sets HBS as the template engine
 
-
 hbs.registerPartials(__dirname + "/views/partials"); //tell HBS which directory we use for partials
-// Makes everything inside of public/ available
-app.use(express.static('public'));
 
+app.use(express.static('public')); // Make everything inside of public/ available
 
-// app.get(path, code)
-//res.render (path, data)
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Connect to DB
+mongoose
+    .connect('mongodb://127.0.0.1:27017/pizza-restaurant')
+    .then(x => {
+        console.log(`Connected! Database name: "${x.connections[0].name}"`);
+    })
+    .catch(err => console.error('Error... ', err));
 
 // Home Page
 app.get("/", (req, res, next) => {
-    res.render('home');
+    res.render("home");
 });
-
 
 // Contact Page
 app.get("/contact", (req, res, next) => {
-    res.render('contact');
+    res.render("contact");
+})
+
+//list of pizzas
+app.get("/pizzas", (req, res, next) => {
+
+    let maxPrice = req.query.maxPrice;
+    maxPrice = Number(maxPrice); //convert to a number
+
+    let filter = {};
+    if (maxPrice) {
+        filter = { price: { $lte: maxPrice } }
+    }
+
+    Pizza.find(filter)
+        .then(pizzasArr => {
+
+            const data = {
+                listOfPizzas: pizzasArr
+            }
+
+            res.render("product-list", data)
+        })
+        .catch(e => console.log("Error getting list of pizzas from DB", e))
 });
 
-//menu
-app.get("/pizzas/margarita", (req, res) =>{
-    
-    const dataMargarita = {
-        title: 'Pizza Margarita',
-        price: 12,
-        recommendedDrink: 'beer',
-        imageFile: 'pizza-margarita.jpg',
-        ingredients: ['mozzarella', 'tomato sauce', 'basilicum'],
-      };
-
-
-    
-    res.render("product", dataMargarita)
+//pizza details
+app.get("/pizzas/:pizzaName", (req, res, next) => {
+    Pizza.findOne({ title: req.params.pizzaName })
+        .then(pizzaDetails => {
+            res.render("product", pizzaDetails);
+        })
+        .catch(e => console.log("Error getting pizza details from DB", e))
 });
 
-app.get("/pizzas/veggie", (req, res) => {
-    
-    const dataVeggie = {
-        title: 'Veggie Pizza',
-        price: 15,
-        recommendedDrink: 'power smoothie',
-        imageFile: 'pizza-veggie.jpg',
-        ingredients: ['cherry tomatoes', 'basilicum', 'olives'],
-      };
-    
-    res.render("product", dataVeggie)
-});
+app.post("/login",(req, res, next) => {
+
+    if(req.body.pwd === "ilovepizza"){
+        res.send("welcome")
+    } else (res.send("sorry, not allowed"))
+ 
+})
+
+
+/*app.get("/pizzas/margarita", (req, res, next) => {
+
+    Pizza.findOne({ title: "margarita" })
+        .then(pizzaDetails => {
+            res.render("product", pizzaDetails)
+        })
+        .catch(e => console.log("Error getting pizza details from DB", e))
+})
+
+app.get("/pizzas/veggie", (req, res, next) => {
+
+    Pizza.findOne({ title: "veggie" })
+        .then(pizzaDetails => {
+            res.render("product", pizzaDetails)
+        })
+        .catch(e => console.log("Error getting pizza details from DB", e))
+})
 
 app.get("/pizzas/seafood", (req, res) => {
+    Pizza.findOne({ title: "seafood" })
+        .then(pizzaDetails => {
+            res.render("product", pizzaDetails)
+        })
+        .catch(e => console.log("Error getting pizza details from DB", e))
+})
 
-    const dataSeafood = {
-        title: 'Seafood Pizza',
-        price: 20,
-        recommendedDrink: 'white wine',
-        imageFile: 'pizza-seafood.jpg',
-        ingredients: ['tomato sauce', 'garlic', 'prawn'],
-      };
-
-    res.render("product", dataSeafood)
-});
+app.get("/pizzas/hawaiian", (req, res) => {
+    Pizza.findOne({ title: "hawaiian" })
+        .then(pizzaDetails => {
+            res.render("product", pizzaDetails)
+        })
+        .catch(e => console.log("Error getting pizza details from DB", e))
+})*/
 
 
-
-app.listen(3000, () => console.log('I am listening! '));
+app.listen(3000, () => console.log('My first app listening on port 3000! '));
